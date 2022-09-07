0353c3e765a66576ce217e1687f5748a6761b0bb
anthony <anthonysa0813@gmail.com>
2022-09-07 16:33:38 -0500

    add the function connectDB with mongoDB

---
+++ db/connectDB.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_CNN);
+    console.log(`se conectó correctamente!`);
+  } catch (err) {
+    console.log("Hubo un error en la conexión");
+  }
+};
+
+module.exports = connectDB;
